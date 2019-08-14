000bc2bcdad189ae7aa9908f936713abb8850442
smartninja-li <52677222+smartninja-li@users.noreply.github.com>
2019-08-14 10:58:18 +0200

    Add files via upload
    added indices

--- script.py
+++ script.py
@@ -2,8 +2,6 @@ import json #json will help us seamlessly convert between a list and a string
 
 print('Welcome to your contactbook')
 
-#hardcode contacts into that list
-#contacts = ["Alex", "Basti", "Chris"]
 # receiving contacts from the contacts.txt
 with open("contacts.txt", "r") as contacts_file:
     contacts = json.loads(contacts_file.read())
@@ -11,18 +9,21 @@ with open("contacts.txt", "r") as contacts_file:
 #print contacts when the program starts
 print('Your contacts are listed below')
 for contact in contacts:
-    print(contact)
+    print(str(contact["index"]) + ": " + contact["First Name"] + " " + contact["Last Name"])
 
 #now you have to let the user decide between two actions
 while True:
     choice = input("Add contact or show list? (add / list) ")
     if choice == "add": #add contact
-        add_name = input("Add a Name: ")
-        contacts.append(add_name)
-        print("Thank you. " + add_name + " was added to your contacts.")
+        new_contact = {}
+        new_contact["index"] = len(contacts) + 1
+        new_contact["First Name"] = input("enter first name: ")
+        new_contact["Last Name"] = input("enter last name: ")
+        contacts.append(new_contact)
+
     elif choice == "list": #show list
         for contact in contacts:
-            print(contact)
+            print(str(contact["index"]) + ": " + contact["First Name"] + " " + contact["Last Name"])
 
     else:
         print('Please type "add" or "list"')
